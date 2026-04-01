1d02a448fb5508ef2d7ebbb100304925a56d3f5b
Ghala <you@example.com>
2026-04-01 11:17:18 +0300

    "TODO 2: parse arguments"

--- calculator.js
+++ calculator.js
@@ -216,3 +216,8 @@ After completing all TODOs, test your calculator:
 import { add, subtract, multiply, divide } from "./utils/operations.js";
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
+
+
+const operation = process.argv[2];
+const numbers = process.argv.slice(3);
+
